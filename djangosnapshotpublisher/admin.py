18c5a06c09f0a2942d1ec148940f5e4038b1f8a3
Yohan Lebret <yohan.lebret@gmail.com>
2019-03-06 17:45:39 +0000

    make base_release editable while creating, but read only in existing objects

--- djangosnapshotpublisher/admin.py
+++ djangosnapshotpublisher/admin.py
@@ -12,7 +12,13 @@ class ContentReleaseAdmin(admin.ModelAdmin):
     ordering = ['title']
     list_display = ('title', 'version', 'site_code', 'uuid', 'base_release', )
     list_filter = ('site_code', )
+    readonly_fields = ['base_release']
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['base_release']
+        else:
+            return []
 
 admin.site.register(ContentRelease, ContentReleaseAdmin)
 admin.site.register(ReleaseDocument)
